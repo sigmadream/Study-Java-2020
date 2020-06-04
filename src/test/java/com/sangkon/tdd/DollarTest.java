aeb96031245b5e49b1f1bae094095aea2b4ba7fd
Sangkon, Han <sigmadream@gmail.com>
2020-06-04 16:36:25 +0900

    프라이버시
    amount를 private으로 만들기

--- src/test/java/com/sangkon/tdd/DollarTest.java
+++ src/test/java/com/sangkon/tdd/DollarTest.java
@@ -8,10 +8,8 @@ public class DollarTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertThat(product.amount).isEqualTo(10);
-        product = five.times(3);
-        assertThat(product.amount).isEqualTo(15);
+        assertThat(new Dollar(10)).isEqualTo(five.times(2));
+        assertThat(new Dollar(15)).isEqualTo(five.times(3));
     }
 
     @Test
