9c9aae981e9827e4d1e2a3e3228ed854e97aba52
Sangkon, Han <sigmadream@gmail.com>
2020-06-04 16:25:33 +0900

    타락한 객체
    Dollar 부작용?

--- src/test/java/com/sangkon/tdd/DollarTest.java
+++ src/test/java/com/sangkon/tdd/DollarTest.java
@@ -7,8 +7,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class DollarTest {
     @Test
     public void testMultiplication() {
-        Dollar five= new Dollar(5);
-        five.times(2);
-        assertThat(five.amount).isEqualTo(10);
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertThat(product.amount).isEqualTo(10);
+        product = five.times(3);
+        assertThat(product.amount).isEqualTo(15);
     }
 }
