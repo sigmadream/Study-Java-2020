3e3f23f88ed9c93d10d4104779d20e214cbafcd1
Sangkon, Han <sigmadream@gmail.com>
2020-06-05 22:56:30 +0900

    06. 모두를 위한 평등

--- src/main/java/com/sangkon/tdd/Franc.java
+++ src/main/java/com/sangkon/tdd/Franc.java
@@ -2,9 +2,7 @@ package com.sangkon.tdd;
 
 import java.util.Objects;
 
-public class Franc {
-    private int amount;
-
+public class Franc extends Money {
     public Franc(int amount) {
         this.amount = amount;
     }
@@ -13,16 +11,4 @@ public class Franc {
         return new Franc(multiplier * amount);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Franc dollar = (Franc) o;
-        return amount == dollar.amount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
-    }
 }
