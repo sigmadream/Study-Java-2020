dea15725cde0ca49ada413aff8150eba1e899a9b
Sangkon, Han <sigmadream@gmail.com>
2020-06-05 22:50:50 +0900

    05. 솔직히 말하자면

--- src/test/java/com/sangkon/tdd/DollarTest.java
+++ src/test/java/com/sangkon/tdd/DollarTest.java
@@ -6,14 +6,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DollarTest {
     @Test
-    public void testMultiplication() {
+    public void testDollarMultiplication() {
         Dollar five = new Dollar(5);
         assertThat(new Dollar(10)).isEqualTo(five.times(2));
         assertThat(new Dollar(15)).isEqualTo(five.times(3));
     }
 
     @Test
-    public void testEquality() {
+    public void testDollarEquality() {
         assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
         assertThat(new Dollar(5)).isNotEqualTo(new Dollar(6));
     }
