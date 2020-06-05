57be976d6fb1c9b3cdec9765520be74448feecd1
Sangkon Han <sigmadream@gmail.com>
2020-06-05 22:49:22 +0900

    메서드 이름 변경

--- src/test/java/com/sangkon/tdd/FrancTest.java
+++ src/test/java/com/sangkon/tdd/FrancTest.java
@@ -6,14 +6,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FrancTest {
     @Test
-    public void testMultiplication() {
+    public void testFrancMultiplication() {
         Franc five = new Franc(5);
         assertThat(new Franc(10)).isEqualTo(five.times(2));
         assertThat(new Franc(15)).isEqualTo(five.times(3));
     }
 
     @Test
-    public void testEquality() {
+    public void testFrancEquality() {
         assertThat(new Franc(5)).isEqualTo(new Franc(5));
         assertThat(new Franc(5)).isNotEqualTo(new Franc(6));
     }
