0555ef173421d895c5eae06015f9285512abdbd6
Sangkon Han <sigmadream@gmail.com>
2020-06-05 23:24:46 +0900

    테스트 코드 작성 및 코드 추가

--- src/test/java/com/sangkon/tdd/MoneyTest.java
+++ src/test/java/com/sangkon/tdd/MoneyTest.java
@@ -6,11 +6,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MoneyTest {
     @Test
-    public void testEquality() {
-        assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
-        assertThat(new Dollar(5)).isNotEqualTo(new Dollar(6));
-        assertThat(new Franc(5)).isEqualTo(new Franc(5));
-        assertThat(new Franc(5)).isNotEqualTo(new Franc(6));
-        assertThat(new Franc(5)).isNotEqualTo(new Dollar(6));
+    public void testDollarMultiplication(){
+        Money five = Money.dollar(5);
+        assertThat(Money.dollar(10)).isEqualTo(five.times(2));
+        assertThat(Money.dollar(15)).isEqualTo(five.times(3));
     }
+
+    @Test
+    public void testFrancMultiplication(){
+        Money five = new Franc(5);
+        assertThat(Money.franc(10)).isEqualTo(five.times(2));
+        assertThat(Money.franc(15)).isEqualTo(five.times(3));
+    }
+
+    @Test
+    public void testMoneyEquality() {
+        assertThat(Money.dollar(5)).isEqualTo(Money.dollar(5));
+        assertThat(Money.dollar(5)).isNotEqualTo(Money.dollar(6));
+        assertThat(Money.franc(5)).isEqualTo(Money.franc(5));
+        assertThat(Money.franc(5)).isNotEqualTo(Money.franc(6));
+        assertThat(Money.dollar(5)).isNotEqualTo(Money.franc(6));
+    }
+
 }
