bdff9d37d41799421b7f5b3ed9cd4fb915239a5f
lsa-src <ssuney@gmail.com>
2020-06-05 15:51:03 +0900

    06.추상화

--- src/test/java/com/sangkon/tdd/MoneyTest.java
+++ src/test/java/com/sangkon/tdd/MoneyTest.java
@@ -23,7 +23,6 @@ public class MoneyTest {
         Money five = Money.dollar(5);
         assertThat(Money.dollar(10)).isEqualTo(five.times(2));
         assertThat(Money.dollar(15)).isEqualTo(five.times(3));
-
     }
 
 }
