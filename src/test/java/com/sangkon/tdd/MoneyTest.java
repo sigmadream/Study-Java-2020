7683994a878bf3242160f1145f36966a24aa080e
Sangkon, Han <sigmadream@gmail.com>
2020-06-06 00:39:56 +0900

    14. 바꾸기 (#28)

--- src/test/java/com/sangkon/tdd/MoneyTest.java
+++ src/test/java/com/sangkon/tdd/MoneyTest.java
@@ -72,4 +72,22 @@ public class MoneyTest {
         assertThat(Money.dollar(1)).isEqualTo(result);
     }
 
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertThat(Money.dollar(1)).isEqualTo(result);
+    }
+
+    @Test
+    public void testArrayEquals() {
+        assertThat(new Object[]{"abc"}).isEqualTo(new Object[]{"abc"});
+    }
+
+    @Test
+    public void testIdentityRate() {
+        assertThat(1).isEqualTo(new Bank().rate("USD", "USD"));
+    }
+
 }
