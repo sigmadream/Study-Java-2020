1e0455095c3fa243fa6e9c9243d44f62cc691f9e
choibyoungjune <kjune0790@gmail.com>
2020-06-05 16:40:33 +0900

    객체만들기

--- src/main/java/com/sangkon/tdd/Dollar.java
+++ src/main/java/com/sangkon/tdd/Dollar.java
@@ -2,9 +2,7 @@ package com.sangkon.tdd;
 
 import java.util.Objects;
 
-public class Dollar {
-    private int amount;
-
+public class Dollar extends Money {
     public Dollar(int amount) {
         this.amount = amount;
     }
@@ -14,15 +12,7 @@ public class Dollar {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Dollar dollar = (Dollar) o;
-        return amount == dollar.amount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
+    public String currency() {
+        return "USD";
     }
 }
